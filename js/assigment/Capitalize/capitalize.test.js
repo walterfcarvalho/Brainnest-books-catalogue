0d327c059d09ed95c4222f0886a51f30b6c2ae43
Valter f Carvalho <walterfcarvalho@gmail.com>
2023-03-04 16:10:34 -0300

    make test broke

--- js/assigment/Capitalize/capitalize.test.js
+++ js/assigment/Capitalize/capitalize.test.js
@@ -7,9 +7,9 @@ describe('Capitalize Test', () => {
     expect(msg.capitalize()). toBe('A sample text');
   });
   
-  test('Test receive the all in uppercase, return capitalized ', () => {
+  test('Test receive the all in uppercase, return ', () => {
     let msg2 = new Capitalize('SAMPLE text');
-    expect(msg2.capitalize()). toBe('Sample text');
+    expect(msg2.capitalize()). toBe('Sample Text');
   });
   
   test('Test receive empty, return empty', () => {
